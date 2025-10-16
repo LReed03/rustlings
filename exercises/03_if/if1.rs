4a3bd09108c1cb283b0ea0a93733906d67bda115
lreed03 <lreed0383@gmail.com>
2025-10-15 23:15:14 -0600

    Finished work on some more functions

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,4 +1,10 @@
 fn bigger(a: i32, b: i32) -> i32 {
+    if a > b{
+        a
+    }
+    else{
+        b
+    }
     // TODO: Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
     // Do not use:
@@ -8,6 +14,7 @@ fn bigger(a: i32, b: i32) -> i32 {
 
 fn main() {
     // You can optionally experiment here.
+    bigger(5,6);
 }
 
 // Don't mind this for now :)
